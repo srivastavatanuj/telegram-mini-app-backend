237fa49e9c29dfc3212b819b3a5e2b072491f40c
srivastavatanuj <tanuj-srivastava@outlook.com>
2024-08-16 21:30:21 +0530

    task logic update

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics,permissions,views,response,status
 from rest_framework.response import Response
 from users.models import User
+from invites.models import Invites
 from .models import Task,TaskProgress
 from .serializers import TaskSerializer,TaskProgressSerializer
 
@@ -29,11 +30,59 @@ class ListUpdateTask(generics.RetrieveUpdateDestroyAPIView):
 
 class startTask(views.APIView):
     serializer_class=TaskProgressSerializer
-    def post(self,request):
-        taskName=Task.objects.get(id=request.data.get("task"))
-        user=User.objects.get(userid=request.data.get("user"))
 
-        task=TaskProgress.objects.create(task=taskName,user=user)
+    def post(self,request):
+        taskid=request.data.get("task")
+        userid=request.data.get("user")
+        taskName=Task.objects.get(id=taskid)
+        user=User.objects.get(userid=userid)
+
+
+        if taskid == "1":         # joining bonus
+            task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+
+        elif taskid == "2":       # invite 1 friend
+            if len(Invites.objects.filter(FromUser=userid))>1:
+                task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+            else:
+                task=TaskProgress.objects.create(task=taskName,user=user)
+
+        elif taskid == "3":       # invite 5 friend
+            if len(Invites.objects.filter(FromUser=userid))>=5:
+                task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+            else:
+                task=TaskProgress.objects.create(task=taskName,user=user)
+
+        elif taskid == "4":       # maintain 5 day streak
+            if user.streak>=5:
+                task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+            else:
+                task=TaskProgress.objects.create(task=taskName,user=user)
+
+        elif taskid == "5":
+            task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+
+        elif taskid == "6":
+            task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+
+        elif taskid == "7":
+            task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+
+        elif taskid == "8":
+            task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+
+        elif taskid == "9":
+            task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+
+        elif taskid == "10":
+            task=TaskProgress.objects.create(task=taskName,user=user,completed=True)
+
+        else:
+            task=TaskProgress.objects.create(task=taskName,user=user)
+
+
+        user.totalScore+=taskName.points
+        user.save()
         serializer=TaskProgressSerializer(task)
 
         return response.Response(serializer.data,status=status.HTTP_201_CREATED)
