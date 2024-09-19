0cac2cfb7b26e235d8605531c6715633fd4ae3ec
Tanuj <73632086+srivastavatanuj@users.noreply.github.com>
2024-09-19 22:26:44 +0530

    change db to mysql

--- backend/settings.py
+++ backend/settings.py
@@ -85,24 +85,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django_test_db',
-#         'USER': 'test',
-#         'PASSWORD': 'mysql',
-#         'HOST': 'localhost',
-#         'POST': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_test_db',
+        'USER': 'test',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'POST': '3306',
+    }
+}
 
 
 
