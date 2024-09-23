5ab570c66a525293bed84246613ba1a1775293e3
Hidden Beyond <100909410+hiddenbeyond@users.noreply.github.com>
2024-09-23 22:21:16 +0530

    Update models.py

--- invites/models.py
+++ invites/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Invites(models.Model):
-    fromUser=models.IntegerField()
+    fromUser=models.BigIntegerField()
     toUser=models.CharField(max_length=150)
     purchase=models.BooleanField(default=False)
     amount=models.IntegerField(default=0)
