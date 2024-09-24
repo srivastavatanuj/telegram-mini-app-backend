61d7cd17125c62c28aba789e6b6290eca607455f
Hidden Beyond <100909410+hiddenbeyond@users.noreply.github.com>
2024-09-24 08:40:44 +0530

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import *
-
+from .views import ListAllTask, ListTask, startTask, gameTask
 
 urlpatterns = [
-    path('all/',ListAllTask.as_view(),name='alltask'),
-    path('<int:id>/',ListTask.as_view(),name='task'),
-    path('starttask/',startTask.as_view(),name='starttask'),
-    path('updategamepoints/',gameTask.as_view(),name='gametask'),
+    path('all/', ListAllTask.as_view(), name='alltask'),
+    path('<int:id>/', ListTask.as_view(), name='task'),
+    path('starttask/', startTask.as_view(), name='starttask'),
+    path('updategamepoints/', gameTask.as_view(), name='gametask'),
 ]
